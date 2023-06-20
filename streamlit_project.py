a84c2619b4eae37cb631fe994a89205a51c3bdd9
Andrew Rosenswie <68133769+ARosenswie@users.noreply.github.com>
2023-06-20 17:40:25 +0200

    Add files via upload

--- streamlit_project.py
+++ streamlit_project.py
@@ -10,10 +10,7 @@ st.write("1. Kátia M. Barros,")
 st.write("2. Juan Brugada,")
 st.write("3. and Andrew Rosenswie.")
 
-st.subheader("Within this application, we employ a Large Language Model from OpenAI. Provided is the analysis of medical question and answer pairs (insert amount here).")
-
-# proivde a citation for the data sources with hyperlinks
-
+st.subheader("Within this application, we employ a Large Language Model (LLM) from OpenAI. Provided is the analysis of medical question and answer pairs (167,793).")
 
 
 
@@ -23,6 +20,9 @@ st.subheader("Please enter your OpenAI API key below:")
 # insert a text box for users to input a question to the model
 st.subheader("Please enter your medical question below:")
 
+# Provide the output of the model
+st.subheader("The model's answer to your question is:")
+st.write("Answer:")
 
 st.subheader("Data Sources:")
 st.write("1. PubMedQA: Jin et al., 2019")
