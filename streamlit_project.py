c036d46572f07d52c34c750820722c49d7cf8168
Andrew Rosenswie <68133769+ARosenswie@users.noreply.github.com>
2023-06-21 15:41:02 +0200

    Add files via upload

--- streamlit_project.py
+++ streamlit_project.py
@@ -1,4 +1,20 @@
+
+# !pip install langchain==0.0.148
+# !pip install rouge
 import streamlit as st
+from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper, ServiceContext
+from langchain import OpenAI
+import sys
+import os
+from IPython.display import Markdown, display
+
+def ask_ai():
+    index = GPTSimpleVectorIndex.load_from_disk('./Data/index.json')
+    while True:
+        query = input("What do you want to ask? ")
+        response = index.query(query)
+        display(Markdown(f"<b>{response.response}</b>"))
+
 
 st.markdown("# *MAG* (Medical Advice Generator)")
 
@@ -10,19 +26,28 @@ st.write("1. Kátia M. Barros,")
 st.write("2. Juan Brugada,")
 st.write("3. and Andrew Rosenswie.")
 
-st.subheader("Within this application, we employ a Large Language Model (LLM) from OpenAI. Provided is the analysis of medical question and answer pairs (167,793).")
+st.subheader("Within this application, we employ a Large Language Model (LLM) from OpenAI. Provided is the analysis of medical question and answer pairs (570,712).")
 
 
-
-# insert a text box for users to input OpenaAI API key
+# Insert a text box for users to input OpenAI API key
 st.subheader("Please enter your OpenAI API key below:")
+openai_api_key = st.text_input("Enter OpenAI key:")
+
+# Set the OpenAI API key as an environment variable
+os.environ["OPENAI_API_KEY"] = openai_api_key
 
 # insert a text box for users to input a question to the model
 st.subheader("Please enter your medical question below:")
+user_question = st.text_input("Submit your question:")
 
-# Provide the output of the model
-st.subheader("The model's answer to your question is:")
-st.write("Answer:")
+def ask_ai(query):
+    index = GPTSimpleVectorIndex.load_from_disk('./Data/index.json')
+    response = index.query(query)
+    display(Markdown(f"<b>{response.response}</b>"))
+
+# Example usage:
+if user_question:
+    ask_ai(user_question)
 
 st.subheader("Data Sources:")
 st.write("1. PubMedQA: Jin et al., 2019")
